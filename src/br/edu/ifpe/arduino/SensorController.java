2c20dc3323fb306a6600d79f74ed494180dc5d31
Matheus <matheuscientista2013@gmail.com>
2016-12-02 07:29:28 -0300

    commit inicial

---
+++ src/br/edu/ifpe/arduino/SensorController.java
@@ -0,0 +1,34 @@
+package br.edu.ifpe.arduino;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SensorController {
+
+    private static SensorController instance;
+    private Map<Integer, ArduinoConnection> arduinos;
+
+    private SensorController() {
+
+        this.arduinos = new HashMap<Integer, ArduinoConnection>();
+
+        //criar e adicionar conexões sensores
+        ArduinoConnection ac = new ArduinoConnection(1);
+        this.arduinos.put(1, ac);
+
+        ArduinoConnection ac2 = new ArduinoConnection(2);
+        this.arduinos.put(2, ac2);
+    }
+
+    public static SensorController getInstance() {
+        if (instance == null) {
+            instance = new SensorController();
+        }
+        return instance;
+    }
+
+    public ArduinoConnection getArduinoConnection(Integer sensorId) {
+        return this.arduinos.get(sensorId);
+    }
+
+}
